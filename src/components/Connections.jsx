8e95fb2c13facb84a60687d1d63b189bbae1167d
darshanik <kakadedarshani@gmail.com>
2025-02-18 15:29:39 +0530

    connections and requests page

---
+++ src/components/Connections.jsx
@@ -0,0 +1,62 @@
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { BASE_URL } from '../utils/constats'
+import { useDispatch, useSelector } from 'react-redux'
+import { addConnections } from "../utils/connectionSlice"
+
+const Connections = () => {
+    const connections = useSelector((store) => store.connections)
+    console.log("connections", connections)
+    const dispatch = useDispatch()
+    const fetchConnections = async () => {
+        try {
+            const res = await axios.get(BASE_URL + "/user/connections", {
+                withCredentials: true
+            })
+            console.log(res?.data.data)
+            dispatch(addConnections(res.data.data))
+        } catch (error) {
+
+        }
+    }
+
+    useEffect(() => {
+        fetchConnections();
+    }, [])
+
+    if (!connections) return
+    if (connections.length === 0) {
+        return (
+            <div className='flex justify-center my-10'>
+                <h1 className='text-bold text-2xl'>No Connections Found!</h1>
+
+            </div>
+        )
+    }
+
+    return (
+        <div className='text-center my-10'>
+            <h1 className='font-bold text-3xl'>Connections</h1>
+
+            {connections.map((connection) => {
+                const { _id, firstName, lastName, photoUrl, about, gender, age } = connection
+                return (
+                    <div key={_id} className='flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto'>
+                        <div>
+                            <img alt="photo" className='w-20 h-20 rounded-full' src={photoUrl}></img>
+                        </div>
+                        <div className='text-left mx-4'>
+
+                            <h2 className='font-bold text-xl'>{firstName + " " + lastName}</h2>
+                            {age && gender && <p>{age + " " + gender} </p>}
+                            <p>{about}</p>
+                        </div>
+                    </div>
+                )
+            })}
+
+        </div>
+    )
+}
+
+export default Connections
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice"
 import feedReducer from "./feedSlice"
+import connctionReducer from "./connectionSlice"
+import requestReducer from './requestSlice'
 
 const appStore = configureStore({
     reducer: {
         user: userReducer,
-        feed: feedReducer
+        feed: feedReducer,
+        connections: connctionReducer,
+        requests: requestReducer
     }
 })
 
